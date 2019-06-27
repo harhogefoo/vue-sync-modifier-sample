0261c678fd883a614e6b6da880cb8a83c035c19e
harhogefoo <harimanmon@gmail.com>
2019-06-27 23:32:26 +0900

    feat: pattern: not prefer

---
+++ src/components/UserInputForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="$emit('send')">
+    <label>
+      メールアドレス
+      <input type="text" v-model="user.email" />
+    </label>
+    <label>
+      名前
+      <input type="text" v-model="user.name" />
+    </label>
+    <button type="submit" @click="$emit('submit')">登録</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+};
+</script>
---
+++ src/views/New.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <user-input-form :user="user" @submit="createUser" />
+  </div>
+</template>
+
+<script>
+import { defaultUser } from "@/helper/user";
+import UserInputForm from "@/components/UserInputForm.vue";
+export default {
+  components: {
+    UserInputForm
+  },
+  data() {
+    return {
+      user: defaultUser() // { email: '', name: '', ... }
+    };
+  },
+  methods: {
+    createUser() {
+      console.log(this.user);
+    }
+  }
+};
+</script>
